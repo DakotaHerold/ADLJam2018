b9245216d2aa8b2b6cd729750301cf2381f8f952
Dakota Herold <dakota.herold@yahoo.com>
2018-10-21 02:21:22 -0400

    Implemented DataManager for grabbing data from Google Spreadsheet.

---
+++ GameJamProject/Assets/Scripts/Managers/DataManager.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+namespace Jam
+{
+    [Serializable]
+    public enum Category
+    {
+        Animals,
+        VideoGames
+    }
+
+    [Serializable]
+    public enum Group
+    {
+        Individual,
+        Group
+    }
+
+    [Serializable]
+    public class PhraseDataContainer
+    {
+        // NOTE: This must be the same name as the google sheet name!
+        public PhraseData[] WrittenData;
+
+        public static PhraseDataContainer Load(string PhraseDataPath)
+        {
+            string filePath = Path.Combine(Application.streamingAssetsPath, PhraseDataPath + ".json");
+            //Debug.Log("Loading from path: " + filePath); 
+
+            var serialized = File.ReadAllText(filePath);
+            return JsonUtility.FromJson<PhraseDataContainer>(serialized);
+        }
+    }
+
+    [Serializable]
+    public class PhraseData
+    {
+        public Category Category;
+        public Group Group;
+        public string Text;
+    }
+
+
+    public class DataManager : MonoBehaviour
+    {
+        private string WrittenDataPath = "WrittenData";
+
+        private void Start()
+        {
+            PhraseData[] phrases = PhraseDataContainer.Load(WrittenDataPath).WrittenData;
+            //if (phrases == null)
+            //    Debug.Log("Null phrases");
+            //else
+            //{
+            //    foreach(PhraseData phrase in phrases)
+            //    {
+            //        Debug.Log("Category: " + phrase.Category);
+            //        Debug.Log("Type: " + phrase.Group);
+            //        Debug.Log("Text: " + phrase.Text);
+            //        Debug.Log("\n"); 
+            //    }
+            //}
+            
+        }
+
+    }
+}
